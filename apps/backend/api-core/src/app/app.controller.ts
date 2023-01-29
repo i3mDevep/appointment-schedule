64d79b5b09c49251eed9ccd6a3ba3025d8a18e80
i3mDevep <ingmichaelheano10@gmail.com>
2023-01-29 01:12:49 -0500

    create module shared

--- apps/backend/api-core/src/app/app.controller.ts
+++ apps/backend/api-core/src/app/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { SharedService } from 'backend/modules/shared';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly sharedService: SharedService
+  ) {}
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return this.sharedService.getData();
   }
 }
